4285fc569d8565a4f79079affa2ab7a3ce894faa
jcw1031 <jcw001031@gmail.com>
2023-05-11 22:19:07 +0900

    Article 엔티티 개발

---
+++ src/main/java/com/woopaca/simpleblog/domain/Article.java
@@ -0,0 +1,44 @@
+package com.woopaca.simpleblog.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+
+@Entity
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    protected Article() {
+    }
+
+    @Builder
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
