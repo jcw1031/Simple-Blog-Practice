1e5c29abb8d79daf0e9b98e273f076aa0f15a0d9
jcw1031 <jcw001031@gmail.com>
2023-05-12 22:53:24 +0900

    게시글 수정 API 컨트롤러(Controller) 개발

--- src/main/java/com/woopaca/simpleblog/controller/BlogApiController.java
+++ src/main/java/com/woopaca/simpleblog/controller/BlogApiController.java
@@ -3,6 +3,7 @@ package com.woopaca.simpleblog.controller;
 import com.woopaca.simpleblog.domain.Article;
 import com.woopaca.simpleblog.dto.AddArticleRequest;
 import com.woopaca.simpleblog.dto.ArticleResponse;
+import com.woopaca.simpleblog.dto.UpdateArticleRequest;
 import com.woopaca.simpleblog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -54,5 +56,14 @@ public class BlogApiController {
 
         return ResponseEntity.ok()
                 .build();
+    }
+
+    @PutMapping("/api/articles/{id}")
+    public ResponseEntity<Article> updateArticle(@PathVariable long id,
+                                                 @RequestBody UpdateArticleRequest request) {
+        Article updatedArticle = blogService.update(id, request);
+
+        return ResponseEntity.ok()
+                .body(updatedArticle);
     }
 }
