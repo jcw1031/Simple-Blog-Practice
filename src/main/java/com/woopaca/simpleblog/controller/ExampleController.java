8763b397f9774b8b554cbc008690c138bb68649c
jcw1031 <jcw001031@gmail.com>
2023-05-13 18:05:53 +0900

    Thymeleaf 예제 컨트롤러 구현

---
+++ src/main/java/com/woopaca/simpleblog/controller/ExampleController.java
@@ -0,0 +1,39 @@
+package com.woopaca.simpleblog.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
+public class ExampleController {
+
+    @GetMapping("/thymeleaf/example")
+    public String thymeleafExample(Model model) {
+        Person examplePerson = new Person();
+        examplePerson.setId(1L);
+        examplePerson.setName("지찬우");
+        examplePerson.setAge(24);
+        examplePerson.setHobbies(List.of("운동", "코딩"));
+
+        model.addAttribute("person", examplePerson);
+        model.addAttribute("today", LocalDate.now());
+
+        return "example";
+    }
+
+    @Getter
+    @Setter
+    static
+    class Person {
+
+        private Long id;
+        private String name;
+        private int age;
+        private List<String> hobbies;
+    }
+}
