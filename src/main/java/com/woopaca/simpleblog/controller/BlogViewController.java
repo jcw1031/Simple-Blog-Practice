f808af04501445ed102787387f2021b93a9950d5
jcw1031 <jcw001031@gmail.com>
2023-05-13 20:28:16 +0900

    게시글 상세 조회 View 컨트롤러 개발

--- src/main/java/com/woopaca/simpleblog/controller/BlogViewController.java
+++ src/main/java/com/woopaca/simpleblog/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package com.woopaca.simpleblog.controller;
 
+import com.woopaca.simpleblog.domain.Article;
 import com.woopaca.simpleblog.dto.ArticleListViewResponse;
+import com.woopaca.simpleblog.dto.ArticleViewResponse;
 import com.woopaca.simpleblog.service.BlogService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,5 +26,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles);
 
         return "articleList";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
