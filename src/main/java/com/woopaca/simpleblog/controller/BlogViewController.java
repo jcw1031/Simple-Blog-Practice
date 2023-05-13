a6e30d31697115cb53821ad11a73de8fa898a489
jcw1031 <jcw001031@gmail.com>
2023-05-13 20:06:11 +0900

    게시글 목록 조회 View 컨트롤러 개발

---
+++ src/main/java/com/woopaca/simpleblog/controller/BlogViewController.java
@@ -0,0 +1,27 @@
+package com.woopaca.simpleblog.controller;
+
+import com.woopaca.simpleblog.dto.ArticleListViewResponse;
+import com.woopaca.simpleblog.service.BlogService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class BlogViewController {
+
+    private final BlogService blogService;
+
+    @GetMapping("/articles")
+    public String getArticles(Model model) {
+        List<ArticleListViewResponse> articles = blogService.findAll().stream()
+                .map(ArticleListViewResponse::new)
+                .toList();
+        model.addAttribute("articles", articles);
+
+        return "articleList";
+    }
+}
