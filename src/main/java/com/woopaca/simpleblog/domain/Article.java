3bff24fb817d702219ac32d5040b411c9ea94d5b
jcw1031 <jcw001031@gmail.com>
2023-05-15 22:27:59 +0900

    @EntityListeners 애노테이션 추가

--- src/main/java/com/woopaca/simpleblog/domain/Article.java
+++ src/main/java/com/woopaca/simpleblog/domain/Article.java
@@ -2,6 +2,7 @@ package com.woopaca.simpleblog.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -11,12 +12,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
 public class Article {
 
     @Id
