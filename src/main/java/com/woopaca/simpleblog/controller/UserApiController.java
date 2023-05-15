b97a48729a58f2d3a4a6580e9ecb483b34bef58c
jcw1031 <jcw001031@gmail.com>
2023-05-15 21:54:09 +0900

    로그아웃 API 컨트롤러 추가

--- src/main/java/com/woopaca/simpleblog/controller/UserApiController.java
+++ src/main/java/com/woopaca/simpleblog/controller/UserApiController.java
@@ -2,8 +2,14 @@ package com.woopaca.simpleblog.controller;
 
 import com.woopaca.simpleblog.dto.AddUserRequest;
 import com.woopaca.simpleblog.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -15,6 +21,14 @@ public class UserApiController {
     @PostMapping("/user")
     public String signUp(AddUserRequest request) {
         userService.save(request);
+        return "redirect:/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        securityContextLogoutHandler.logout(request, response, securityContext.getAuthentication());
         return "redirect:/login";
     }
 }
