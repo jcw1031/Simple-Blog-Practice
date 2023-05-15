a7254c1ee9fec9438c4026b5bb62c69dade15fdc
jcw1031 <jcw001031@gmail.com>
2023-05-15 21:09:13 +0900

    회원가입 비즈니스 로직 개발

---
+++ src/main/java/com/woopaca/simpleblog/service/UserService.java
@@ -0,0 +1,23 @@
+package com.woopaca.simpleblog.service;
+
+import com.woopaca.simpleblog.domain.User;
+import com.woopaca.simpleblog.dto.AddUserRequest;
+import com.woopaca.simpleblog.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest request) {
+        return userRepository.save(User.builder()
+                .email(request.getEmail())
+                .password(bCryptPasswordEncoder.encode(request.getPassword()))
+                .build()).getId();
+    }
+}
