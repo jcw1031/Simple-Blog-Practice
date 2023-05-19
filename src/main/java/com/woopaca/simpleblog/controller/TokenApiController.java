a75a97765615602fc5e3db6591c264eb84b5b26d
jcw1031 <jcw001031@gmail.com>
2023-05-19 13:47:30 +0900

    access token 발급 요청 API 컨트롤러 개발

---
+++ src/main/java/com/woopaca/simpleblog/controller/TokenApiController.java
@@ -0,0 +1,28 @@
+package com.woopaca.simpleblog.controller;
+
+import com.woopaca.simpleblog.dto.CreateAccessTokenRequest;
+import com.woopaca.simpleblog.dto.CreateAccessTokenResponse;
+import com.woopaca.simpleblog.service.TokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class TokenApiController {
+
+    private final TokenService tokenService;
+
+    @PostMapping("/api/token")
+    public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken(
+            @RequestBody CreateAccessTokenRequest request
+    ) {
+        String newAccessToken = tokenService.createNewAccessToken(request.getRefreshToken());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(new CreateAccessTokenResponse(newAccessToken));
+    }
+}
