cc76b329e8eb8fd7c66afce6324047783225c82f
jcw1031 <jcw001031@gmail.com>
2023-05-19 13:25:44 +0900

    refresh token 조회 및 전달 로직 추가

---
+++ src/main/java/com/woopaca/simpleblog/service/RefreshTokenService.java
@@ -0,0 +1,18 @@
+package com.woopaca.simpleblog.service;
+
+import com.woopaca.simpleblog.domain.RefreshToken;
+import com.woopaca.simpleblog.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+}
