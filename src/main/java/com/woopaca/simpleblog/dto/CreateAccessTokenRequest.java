3612486d0c9b639a697bdb19ae228f271220e4a7
jcw1031 <jcw001031@gmail.com>
2023-05-19 13:41:10 +0900

    access token 발급 요청 및 응답 데이터 전달용 DTO 구현

---
+++ src/main/java/com/woopaca/simpleblog/dto/CreateAccessTokenRequest.java
@@ -0,0 +1,11 @@
+package com.woopaca.simpleblog.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateAccessTokenRequest {
+
+    private String refreshToken;
+}
