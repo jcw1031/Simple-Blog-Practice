b995291ca13e8e25de46fff095f0e5f4277bc628
jcw1031 <jcw001031@gmail.com>
2023-05-24 13:40:07 +0900

    게시글 수정, 삭제 시 Author 검증

--- src/main/java/com/woopaca/simpleblog/service/BlogService.java
+++ src/main/java/com/woopaca/simpleblog/service/BlogService.java
@@ -5,6 +5,7 @@ import com.woopaca.simpleblog.dto.AddArticleRequest;
 import com.woopaca.simpleblog.dto.UpdateArticleRequest;
 import com.woopaca.simpleblog.repository.BlogRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,7 +31,11 @@ public class BlogService {
     }
 
     public void delete(long id) {
-        blogRepository.deleteById(id);
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        authorizeArticleAuthor(article);
+        blogRepository.delete(article);
     }
 
     @Transactional
@@ -38,8 +43,16 @@ public class BlogService {
         Article article = blogRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
 
+        authorizeArticleAuthor(article);
         article.update(request.getTitle(), request.getContent());
 
         return article;
+    }
+
+    private void authorizeArticleAuthor(Article article) {
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+        if (!article.getAuthor().equals(userName)) {
+            throw new IllegalArgumentException("not authorized");
+        }
     }
 }
