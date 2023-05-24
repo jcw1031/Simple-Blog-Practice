040c604dcc604b12c553c0b6012bec148a9ad11a
jcw1031 <jcw001031@gmail.com>
2023-05-24 13:36:10 +0900

    OAuth 로그인 뷰 추가 및 JS 수정

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -2,14 +2,19 @@ const deleteButton = document.getElementById('delete-btn');
 
 if (deleteButton) {
     deleteButton.addEventListener('click', event => {
-        let id = document.getElementById('article-id').value;
-        fetch(`/api/articles/${id}`, {
-            method: 'DELETE'
-        })
-            .then(() => {
-                alert('삭제가 완료되었습니다.');
-                location.replace('/articles');
-            });
+        const id = document.getElementById('article-id').value;
+
+        function success() {
+            alert('삭제가 완료되었습니다.');
+            location.replace('/articles');
+        }
+
+        function fail() {
+            alert('삭제 실패했습니다.');
+            location.replace('/articles');
+        }
+
+        httpRequest('DELETE', '/api/articles/' + id, null, success, fail);
     });
 }
 
@@ -20,26 +25,28 @@ if (modifyButton) {
         let params = new URLSearchParams(location.search);
         let id = params.get('id');
 
-        fetch(`/api/articles/${id}`, {
-            method: 'PUT',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                title: document.getElementById('title').value,
-                content: document.getElementById('content').value
-            })
-        })
-            .then(() => {
-                alert('수정이 완료되었습니다.');
-                location.replace(`/articles/${id}`);
-            });
-    });
+        const body = JSON.stringify({
+            title: document.getElementById('title').value,
+            content: document.getElementById('content').value
+        });
+
+        function success() {
+            alert('수정 완료되었습니다.');
+            location.replace('/articles/' + id);
+        }
+
+        function fail() {
+            alert('수정 실패했습니다.');
+            location.replace('/articles/' + id);
+        }
+
+        httpRequest('PUT', '/api/articles/' + id, body, success, fail);
+    })
 }
 
 const createButton = document.getElementById('create-btn');
 
-if (createButton) {
+/*if (createButton) {
     createButton.addEventListener('click', event => {
         fetch('/api/articles', {
             method: 'POST',
@@ -55,5 +62,80 @@ if (createButton) {
                 alert('등록 완료되었습니다.');
                 location.replace('/articles');
             });
+    });
+}*/
+
+if (createButton) {
+    createButton.addEventListener("click", event => {
+        const body = JSON.stringify({
+            title: document.getElementById('title').value,
+            content: document.getElementById('content').value
+        });
+
+        function success() {
+            alert('등록 완료되었습니다.');
+            location.replace('/articles');
+        }
+
+        function fail() {
+            alert('등록 실패했습니다.');
+            location.replace('/articles');
+        }
+
+        httpRequest('POST', '/api/articles', body, success, fail);
+    });
+}
+
+function getCookie(key) {
+    let result = null;
+    const cookie = document.cookie.split(';');
+    cookie.some(function (item) {
+        item = item.replace(' ', '');
+        const dic = item.split('=');
+
+        if (key === dic[0]) {
+            result = dic[1];
+            return true;
+        }
+    });
+
+    return result;
+}
+
+function httpRequest(method, url, body, success, fail) {
+    fetch(url, {
+        method: method,
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+            'Content-Type': 'application/json'
+        },
+        body: body
+    }).then(response => {
+        if (response.status === 200 || response.status === 201) {
+            return success;
+        }
+
+        const refresh_token = getCookie('refresh_token');
+        if (response.status === 401 && refresh_token) {
+            fetch('/api/token', {
+                method: 'POST',
+                headers: {
+                    Authorization: 'Bearer ' + localStorage.getItem('access_token'),
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    refresh_token: getCookie('refresh_token')
+                })
+            }).then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+            }).then(result => {
+                localStorage.setItem('access_token', result.accessToken);
+                httpRequest(method, url, body, success, fail);
+            }).catch(error => fail);
+        } else {
+            return fail;
+        }
     });
 }
